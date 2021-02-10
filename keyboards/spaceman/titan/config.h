e99dad7131a16270e142ad8ea999a54ba7b6d7da
Spaceman <rionlion100@gmail.com>
2021-02-10 14:32:39 -0500

    Update key positions

--- keyboards/spaceman/titan/config.h
+++ keyboards/spaceman/titan/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 5
-#define MATRIX_COLS 15
+#define MATRIX_COLS 16
 
 /* Row pin configuration*/
 #define MATRIX_ROW_PINS { B4, B5, B6, C6, F7 }
